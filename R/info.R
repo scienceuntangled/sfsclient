9b183afdd54ddc3c3d90f9aa9b0ba3ea0a7c93cd
Ben Raymond <ben@theraymonds.org>
2023-06-09 20:44:23 +1000

    add sfs_fs_info func

--- R/info.R
+++ R/info.R
@@ -18,3 +18,26 @@ sfs_info <- function(url, verbose = sfs_verbose()) {
         NULL
     }
 }
+
+#' Retrieve File Station information
+#'
+#' @references <https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/FileStation/All/enu/Synology_File_Station_API_Guide.pdf>
+#' @param sfs sfsclient: as returned by [sfs_connect()]
+#' @param verbose logical: give debug info?
+#'
+#' @return A list, with information potentially including
+#' * is_manager - if the logged-in user is an administrator
+#' * support_virtual_protocol - types of virtual file system which the logged user is able to mount on (CIFS, NFS, ISO)
+#' * support_sharing - if the logged-in user can share files/folders
+#' * hostname - host name
+#'
+#' @export
+sfs_fs_info <- function(sfs, verbose = sfs_verbose()) {
+    url <- "entry.cgi?api=SYNO.FileStation.Info&version=2&method=get"
+    rsp <- sfsget(sfs, url, verbose = verbose)
+    if (status_code(rsp) == 200) {
+        fromJSON(content(rsp, as = "text"), simplifyDataFrame = TRUE)$data
+    } else {
+        NULL
+    }
+}
