7331951cc68808d90055b7ae8833f8c5c0dd7a9e
Ben Raymond <ben@theraymonds.org>
2023-06-10 09:09:06 +1000

    add out_dir parm for download v0.1.0

--- R/download.R
+++ R/download.R
@@ -3,6 +3,7 @@
 #' @references <https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/FileStation/All/enu/Synology_File_Station_API_Guide.pdf>
 #' @param sfs sfsclient: as returned by [sfs_connect()]
 #' @param path character: one or more file or folder paths (each starting with a shared folder. Probably needs to start with a "/")
+#' @param out_dir string: destination directory. If not provided, a temporary directory will be used
 #' @param mode string: "download" (default) or "open"
 #' @param progress logical: show download progress?
 #' @param verbose logical: give debug info?
@@ -10,18 +11,15 @@
 #' @return A data.frame or `NULL`
 #'
 #' @export
-sfs_download <- function(sfs, path, mode = "download", progress = TRUE, verbose = sfs_verbose()) {
+sfs_download <- function(sfs, path, out_dir, mode = "download", progress = TRUE, verbose = sfs_verbose()) {
+    if (missing(out_dir)) {
+        out_dir <- tempfile()
+        dir.create(out_dir)
+    }
+    if (!dir.exists(out_dir)) stop("out_dir does not exist: ", out_dir)
     mode <- match.arg(mode, c("download", "open"))
     url <- paste0("entry.cgi?api=SYNO.FileStation.Download&version=2&method=download&path=", sqb(path), "&mode=\"", mode, "\"")
-    outfile <- if (length(path) == 1) {
-                   tryCatch({
-                       tempd <- tempfile()
-                       dir.create(tempd)
-                       file.path(tempd, basename(path))
-                   }, error = function(e) tempfile())
-               } else {
-                   tempfile()
-               }
+    outfile <- if (length(path) == 1) file.path(out_dir, basename(path)) else tempfile(tmpdir = out_dir)
     rsp <- sfsget(sfs, url, outfile = outfile, progress = progress, verbose = verbose)
     if (rsp$status_code == 200) {
         ## rename the output file if we can
